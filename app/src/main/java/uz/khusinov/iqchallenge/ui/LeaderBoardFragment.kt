6381ac3d0a729261e5fe03c8453198f147358f16
Beko404 <abekdiyor@gmail.com>
2024-03-26 17:59:28 +0500

    rating list

--- app/src/main/java/uz/khusinov/iqchallenge/ui/LeaderBoardFragment.kt
+++ app/src/main/java/uz/khusinov/iqchallenge/ui/LeaderBoardFragment.kt
@@ -1,60 +1,42 @@
 package uz.khusinov.iqchallenge.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import uz.khusinov.iqchallenge.R
+import uz.khusinov.iqchallenge.adapters.RatingAdapter
+import uz.khusinov.iqchallenge.databinding.FragmentLeaderBoardBinding
+import uz.khusinov.iqchallenge.models.Rating
+import uz.khusinov.iqchallenge.utills.viewBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class LeaderBoardFragment : Fragment(R.layout.fragment_leader_board) {
+    private val binding by viewBinding { FragmentLeaderBoardBinding.bind(it) }
+    private val adapter by lazy { RatingAdapter() }
+    private var dataList = mutableListOf<Rating>()
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LeaderBoardFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class LeaderBoardFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loadData()
+        setupUI()
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private fun loadData() {
+        dataList.clear()
+        dataList.add(Rating("1", "Bekdiyor", 278))
+        dataList.add(Rating("2", "Shaxriyor", 269))
+        dataList.add(Rating("3", "Rustam", 255))
+        dataList.add(Rating("4", "Asad", 241))
+        dataList.add(Rating("5", "Lazizbek", 226))
+        dataList.add(Rating("6", "Elmurod", 217))
+        adapter.submitList(dataList)
+    }
+
+    private fun setupUI() = with(binding) {
+        toolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
         }
-    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_leader_board, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LeaderBoardFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LeaderBoardFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        ratingRecycler.adapter = adapter
     }
 }
