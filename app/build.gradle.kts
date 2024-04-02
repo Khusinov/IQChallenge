7cc6c2b3928920733754faac8d1bf3580dcf2118
usermac <shakhriyordev@gmail.com>
2024-04-02 11:21:04 +0500

    firebase data getting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
 }
 
 android {
@@ -47,6 +49,8 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("com.google.firebase:firebase-crashlytics:18.6.3")
+    implementation("com.google.firebase:firebase-firestore:24.11.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/uz/khusinov/iqchallenge/ui/QuizFragment.kt
+++ app/src/main/java/uz/khusinov/iqchallenge/ui/QuizFragment.kt
@@ -1,24 +1,61 @@
 package uz.khusinov.iqchallenge.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import uz.khusinov.iqchallenge.R
 import uz.khusinov.iqchallenge.databinding.FragmentQuizBinding
+import uz.khusinov.iqchallenge.models.Quiz
 import uz.khusinov.iqchallenge.utills.viewBinding
 
 class QuizFragment : Fragment(R.layout.fragment_quiz) {
-   private val binding by viewBinding { FragmentQuizBinding.bind(it) }
+    private val binding by viewBinding { FragmentQuizBinding.bind(it) }
+
+    val db = Firebase.firestore
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupUI()
     }
 
-    private fun setupUI() = with(binding){
+    private fun setupUI() = with(binding) {
         result.setOnClickListener {
             findNavController().navigate(R.id.action_quizFragment_to_resultFragment)
         }
+        val quizzes = mutableListOf<Quiz>()
+
+        db.collection("quiz")
+            .get()
+            .addOnSuccessListener { result ->
+                quizzes.clear()
+                for (i in result) {
+                    val quiz = Quiz(
+                        i.data["id"].toString().toInt(),
+                        i.data["question"].toString(),
+                        i.data["questionImg"].toString(),
+                        i.data["level"].toString().toInt(),
+                        i.data["answer1"].toString(),
+                        i.data["answer2"].toString(),
+                        i.data["answer3"].toString(),
+                        i.data["answer4"].toString(),
+                        i.data["answer5"].toString(),
+                        i.data["answer6"].toString(),
+                        i.data["correctAnswer"].toString()
+                    )
+                    quizzes.add(quiz)
+                }
+                Log.d("TAG", "setupUI: tayyor ${quizzes.size} ")
+            }
+
+            .addOnFailureListener { e ->
+                Log.w("TAG", "Error adding document", e)
+            }
+
+
     }
 }
